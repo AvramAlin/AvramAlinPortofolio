e11e3b9f1b08939e0686adba5fd46a7bc768a53e
Avram Alin Ioan <avramalinmedia@gmail.com>
2024-10-30 19:50:49 +0200

    Polished the nav-bar

--- scriptPortofolio.js
+++ scriptPortofolio.js
@@ -3,11 +3,20 @@
     document.getElementById("aboutmeNav").addEventListener("click", function() {
         document.getElementById("aboutMe").scrollIntoView({ behavior: 'smooth' });
     });
-
-
-
     document.getElementById("educationNav").addEventListener("click", function() {
         document.getElementById("education").scrollIntoView({ behavior: 'smooth' });
+    });
+    document.getElementById("experienceNav").addEventListener("click", function() {
+        document.getElementById("experienceCert").scrollIntoView({ behavior: 'smooth' });
+    });
+    document.getElementById("projectsNav").addEventListener("click", function() {
+        document.getElementById("personalProjects").scrollIntoView({ behavior: 'smooth' });
+    });
+    document.getElementById("hobbiesNav").addEventListener("click", function() {
+        document.getElementById("hobbiesId").scrollIntoView({ behavior: 'smooth' });
+    });
+    document.getElementById("skillsNav").addEventListener("click", function() {
+        document.getElementById("skillsId").scrollIntoView({ behavior: 'smooth' });
     });
 
 
