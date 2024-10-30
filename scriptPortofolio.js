ac623a32937bc10e0c9ea209d5f1c0a533690595
Avram Alin Ioan <avramalinmedia@gmail.com>
2024-10-30 18:23:31 +0200

    Added the possibility to download the CV when pressing the DOWNLOADCV button

--- scriptPortofolio.js
+++ scriptPortofolio.js
@@ -1,18 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("startButton").addEventListener("click", function() {
-        document.getElementById("aboutMe").scrollIntoView({ behavior: 'smooth' });
-    });
-});
 
-document.addEventListener("DOMContentLoaded", function() {
+
     document.getElementById("aboutmeNav").addEventListener("click", function() {
         document.getElementById("aboutMe").scrollIntoView({ behavior: 'smooth' });
     });
-});
 
-document.addEventListener("DOMContentLoaded", function() {
+
+
     document.getElementById("educationNav").addEventListener("click", function() {
         document.getElementById("education").scrollIntoView({ behavior: 'smooth' });
     });
+
+
+document.getElementById("downloadCV").addEventListener("click",function(){
+    const link = document.createElement("a");
+    link.href = "AvramAlinResumeGit.pdf";
+    link.download = "AvramAlinResume.pdf";
+
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
 });
 
