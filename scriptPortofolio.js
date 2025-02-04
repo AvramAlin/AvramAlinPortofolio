01a7f9729f1378620e634998c0cc5e2d4bc75542
Avram Alin Ioan <avramalinmedia@gmail.com>
2025-02-04 14:05:00 +0200

    Updated version with newest projects

--- scriptPortofolio.js
+++ scriptPortofolio.js
@@ -1,34 +1,34 @@
-
-
-    document.getElementById("aboutmeNav").addEventListener("click", function() {
-        document.getElementById("aboutMe").scrollIntoView({ behavior: 'smooth' });
-    });
-    document.getElementById("educationNav").addEventListener("click", function() {
-        document.getElementById("education").scrollIntoView({ behavior: 'smooth' });
-    });
-    document.getElementById("experienceNav").addEventListener("click", function() {
-        document.getElementById("experienceCert").scrollIntoView({ behavior: 'smooth' });
-    });
-    document.getElementById("projectsNav").addEventListener("click", function() {
-        document.getElementById("personalProjects").scrollIntoView({ behavior: 'smooth' });
-    });
-    document.getElementById("hobbiesNav").addEventListener("click", function() {
-        document.getElementById("hobbiesId").scrollIntoView({ behavior: 'smooth' });
-    });
-    document.getElementById("skillsNav").addEventListener("click", function() {
-        document.getElementById("skillsId").scrollIntoView({ behavior: 'smooth' });
-    });
-
-
-document.getElementById("downloadCV").addEventListener("click",function(){
-    const link = document.createElement("a");
-    link.href = "./AvramAlinResumeGit.pdf";
-    link.download = "AvramAlinResumeDownload.pdf";
-
-    document.body.appendChild(link);
-
-    link.click();
-
-    document.body.removeChild(link);
+document.getElementById("aboutmeNav").addEventListener("click", function () {
+  document.getElementById("aboutMe").scrollIntoView({ behavior: "smooth" });
+});
+document.getElementById("educationNav").addEventListener("click", function () {
+  document.getElementById("education").scrollIntoView({ behavior: "smooth" });
+});
+document.getElementById("experienceNav").addEventListener("click", function () {
+  document
+    .getElementById("experienceCert")
+    .scrollIntoView({ behavior: "smooth" });
+});
+document.getElementById("projectsNav").addEventListener("click", function () {
+  document
+    .getElementById("personalProjects")
+    .scrollIntoView({ behavior: "smooth" });
+});
+document.getElementById("hobbiesNav").addEventListener("click", function () {
+  document.getElementById("hobbiesId").scrollIntoView({ behavior: "smooth" });
+});
+document.getElementById("skillsNav").addEventListener("click", function () {
+  document.getElementById("skillsId").scrollIntoView({ behavior: "smooth" });
 });
 
+document.getElementById("downloadCV").addEventListener("click", function () {
+  const link = document.createElement("a");
+  link.href = "./AvramAlinResumeUpdated.pdf";
+  link.download = "AvramAlinResumeDownload.pdf";
+
+  document.body.appendChild(link);
+
+  link.click();
+
+  document.body.removeChild(link);
+});
